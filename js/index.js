e6625cfd24517556113c8956d5aeadbe24187a7b
Labutkina Yulia <labutkinayuliaa@gmail.com>
2020-08-12 20:37:06 +0300

    added homework 6

---
+++ js/index.js
@@ -0,0 +1,162 @@
+"use strict";
+
+import users from './users.js';
+
+// ****** Task - 1 ****** 
+
+//Получить массив имен всех пользователей (поле name).
+
+
+const getUserNames = users => users.map(user => user.name);
+  
+console.group(`Task-1`);
+console.log(getUserNames(users));
+console.groupEnd();
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+
+
+// ****** Task - 2 ****** 
+
+// Получить массив объектов пользователей по цвету глаз (поле eyeColor).
+
+const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color);
+
+console.group(`Task-2`);
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+console.groupEnd();
+
+
+
+// ****** Task - 3 ****** 
+
+// Получить массив имен пользователей по полу (поле gender).
+
+// const getUsersWithGender = (users, gender) => {
+//   return users.reduce((acc, item) => {
+//     if(item.gender === gender) {
+//       acc.push(item.name);
+//     }
+
+//     return acc;
+//   }, [])
+// };
+
+const getUsersWithGender = (users, gender) => users.filter(user => user.gender === gender).map(user => user.name);
+
+console.group(`Task-3`);
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+console.groupEnd();
+
+
+
+
+// ****** Task - 4 ****** 
+
+// Получить массив только неактивных пользователей (поле isActive).
+
+const getInactiveUsers = users => users.filter(user => !user.isActive);
+
+console.group(`Task-4`);
+console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+console.groupEnd();
+
+
+
+// ****** Task - 5 ****** 
+
+
+// Получить пользоваля (не массив) по email (поле email, он уникальный).
+
+const getUserWithEmail = (users, email) => users.find(user => user.email === email);
+
+console.group(`Task-5`);
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+console.groupEnd();
+
+
+
+
+// ****** Task - 6 ****** 
+
+
+// Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
+
+const getUsersWithAge = (users, min, max) => users.filter(user => user.age > min && user.age < max);
+
+console.group(`Task-6`);
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+console.groupEnd();
+
+
+
+
+// ****** Task - 7 ****** 
+
+// Получить общую сумму баланса (поле balance) всех пользователей.
+
+const calculateTotalBalance = users => users.reduce((acc, user) => acc + user.balance, 0);
+
+console.group(`Task-7`);
+console.log(calculateTotalBalance(users)); // 20916
+console.groupEnd();
+
+
+
+
+// ****** Task - 8 ****** 
+
+
+// Массив имен всех пользователей у которых есть друг с указанным именем.
+
+const getUsersWithFriend = (users, friendName) => users.filter(({ friends }) => friends.includes(friendName)).map(user => user.name);
+
+// const getUsersWithFriend = (users, friendName) => {
+
+//   return users.reduce(function(acc, friend) {
+
+//    if (friend.friends.some(friend => friend === friendName)) {
+//      acc.push(friend.name);
+//    }
+
+//    return acc;
+// }, []);
+// };
+
+console.group(`Task-8`);
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+console.groupEnd();
+
+
+
+
+// ****** Task - 9 ****** 
+
+// Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
+
+const getNamesSortedByFriendsCount = users => users.sort((a, b) => a.friends.length - b.friends.length).map(user => user.name);
+
+console.group(`Task-9`);
+console.log(getNamesSortedByFriendsCount(users));
+console.groupEnd();
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+
+
+
+// ****** Task - 10 ****** 
+
+// Получить массив всех умений всех пользователей (поле skills), при этом не должно быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
+
+const getSortedUniqueSkills = users => {
+  return users.reduce((acc, user) => [...acc, ...user.skills], []).filter((skill, index, arr) => arr.indexOf(skill) === index).sort();
+};
+
+console.group(`Task-10`);
+console.log(getSortedUniqueSkills(users));
+console.groupEnd();
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
